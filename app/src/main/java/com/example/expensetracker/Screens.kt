c6839034ae07416b4b4ac44e7bce060c25d7126b
Cristian D Ullua <cristiandullua@byui.edu>
2024-12-10 18:23:37 -0300

    display currency in record item

--- app/src/main/java/com/example/expensetracker/Screens.kt
+++ app/src/main/java/com/example/expensetracker/Screens.kt
@@ -48,11 +48,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import kotlinx.coroutines.flow.Flow
 import java.sql.Date
